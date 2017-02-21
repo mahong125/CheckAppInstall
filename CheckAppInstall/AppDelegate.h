a58c249dc48987cfae43623cc93bda62d93b38be
mahong <hong_xiazhi@163.com>
2017-02-21 11:48:40 +0800

    add by mahong

---
+++ CheckAppInstall/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CheckAppInstall
+//
+//  Created by mahong on 17/2/21.
+//  Copyright © 2017年 mahong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CheckAppInstall/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CheckAppInstall
+//
+//  Created by mahong on 17/2/21.
+//  Copyright © 2017年 mahong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
